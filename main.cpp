2fb6737716117ee9ad203214e7fb61c9645672a9
tatsuki-m <tatsukitatsuki0923@gmail.com>
2017-01-14 03:17:42 +0900

    read dpi data one by one

--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
-#include <door_bridge/door_bridge.h>
-#include <door_unix_domain_socket_client/unix_domain_socket_client.h>
-#include <door_shared_memory/dpi.h>
+#include <fstream>
+#include <iomanip>
+
+#include "door_bridge/door_bridge.h"
+#include "door_unix_domain_socket_client/unix_domain_socket_client.h"
+#include "door_shared_memory/dpi.h"
+#include "door_shared_memory/shared_packet_information.h"
+
+const int MAX_COUNT = 1002;
+std::string BASE_RECORDER_DIR = "/tmp/recorder/";
+std::string ENV = "native";
 
 int
 main() {
-    DoorBridge bridge = DoorBridge();
-    Dpi *dpi;
-    std::string keyword = "hoge";
-    std::cout << "dpi address:" << &dpi << std::endl;
-    bool is_success = bridge.getAllInformation(dpi, keyword);
+    // init
+    unsigned int counter = 0;
+    struct timespec startTime, endTime;
+    Dpi *dpi = NULL;
 
-    if (is_success) {
-        std::cout << "success" << std::endl;
-    } else {
-        std::cout << "false" << std::endl;
+    // create timer
+    std::string fileName = BASE_RECORDER_DIR + ENV + "_" + std::to_string(SharedPacketInformation::getSharedDataSize()) + "_throuput.csv";
+    std::ofstream ofs(fileName.c_str());
+    ofs << "start_time, end_time, interval" << std::endl;
+
+    // init DoorBridge
+    DoorBridge bridge = DoorBridge();
+
+
+    if (bridge.callDoorWithSem()) {
+        while(counter<MAX_COUNT) {
+            bridge.getPacketDataWithSem(dpi);
+            counter++;
+        }
     }
+    ofs << std::setfill('0') << std::setw(6) << startTime.tv_nsec << ",";
+    ofs << std::setfill('0') << std::setw(6) << endTime.tv_nsec << ",";
+    ofs << endTime.tv_nsec - startTime.tv_nsec << std::endl;
+
     delete dpi;
     return 0;
 };

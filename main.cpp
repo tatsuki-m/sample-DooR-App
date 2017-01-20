133ca94cd2a1a2265e0b17b00782cd702a0854ac
tatsuki-m <tatsukitatsuki0923@gmail.com>
2017-01-20 19:11:19 +0900

    change due to lib

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 
 #include "door_bridge/door_bridge.h"
 #include "door_unix_domain_socket_client/unix_domain_socket_client.h"
-#include "door_shared_memory/dpi.h"
-#include "door_shared_memory/shared_packet_information.h"
+#include "door_ipc/dpi.h"
+#include "door_ipc/shared_packet_information.h"
 
 const int MAX_COUNT = 1000;
 std::string BASE_RECORDER_DIR = "/tmp/recorder/";
@@ -18,24 +18,22 @@ main() {
     // init
     unsigned int counter = 0;
     struct timespec startTime, endTime;
-    Dpi *dpi;
+    // init DoorBridge
+    DoorBridge bridge = DoorBridge();
 
+    Dpi *dpi;
     // create timer
     std::string fileName = BASE_RECORDER_DIR + ENV + "_" + std::to_string(SharedPacketInformation::getSharedDataSize()) + "_throuput.csv";
     std::ofstream ofs(fileName.c_str());
     ofs << "start_time, end_time, interval" << std::endl;
 
-    // init DoorBridge
-    DoorBridge bridge = DoorBridge();
 
     clock_gettime(CLOCK_MONOTONIC, &startTime);
-    if (bridge.callDoorWithSem()) {
-        std::cout << "reading start" << std::endl;
-        bridge.waitDoorNotification();
-        while(counter<MAX_COUNT) {
-            bridge.getPacketDataWithSem(dpi);
-            counter++;
-        }
+    bridge.callDoorWithSem();
+    std::cout << "reading start" << std::endl;
+    while(counter<MAX_COUNT) {
+        bridge.getPacketDataWithSem(dpi);
+        counter++;
     }
     clock_gettime(CLOCK_MONOTONIC, &endTime);
     ofs << std::setfill('0') << std::setw(6) << startTime.tv_nsec << ",";
